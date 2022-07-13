a3832fd9f3c02ed4c922717c304f2d8c9160cf48
east4ming <cuikaidong@foxmail.com>
2022-07-13 16:29:43 +0800

    format

--- Chapter04/netaddr_module.py
+++ Chapter04/netaddr_module.py
@@ -30,18 +30,21 @@ def operate_on_ip_network(ipnet):
     net_attributes = []
     net = IPNetwork(ipnet)
 
-    net_attributes.append("Network IP Address is " + str(net.network) + " and Network Mask is " + str(net.netmask))
+    net_attributes.append("Network IP Address is " + str(net.network) +
+                          " and Network Mask is " + str(net.netmask))
 
     net_attributes.append("The Broadcast is " + str(net.broadcast))
 
     net_attributes.append("IP Version is " + str(net.version))
-    net_attributes.append("Information known about this network is " + str(net.info))
+    net_attributes.append(
+        "Information known about this network is " + str(net.info))
 
     net_attributes.append("The IPv6 representation is " + str(net.ipv6()))
 
     net_attributes.append("The Network size is " + str(net.size))
 
-    net_attributes.append("Generating a list of ip addresses inside the subnet")
+    net_attributes.append(
+        "Generating a list of ip addresses inside the subnet")
 
     for ip in net:
         net_attributes.append("\t" + str(ip))
@@ -49,8 +52,8 @@ def operate_on_ip_network(ipnet):
     return "\n".join(net_attributes)
 
 
-ipaddr = raw_input("Please Enter the IP Address: ")
-print check_ip_address(ipaddr)
+ipaddr = input("Please Enter the IP Address: ")
+print(check_ip_address(ipaddr))
 
-ipnet = raw_input("Please Enter the IP Network: ")
-print operate_on_ip_network(ipnet)
+ipnet = input("Please Enter the IP Network: ")
+print(operate_on_ip_network(ipnet))
--- Chapter04/netmiko_exception.py
+++ Chapter04/netmiko_exception.py
@@ -12,22 +12,26 @@ device = {
     'secret': 'access123',
 }
 
-print "########## Connecting to Device {0} ############".format(device['ip'])
+print("########## Connecting to Device {0} ############".format(device['ip']))
 try:
     net_connect = ConnectHandler(**device)
     net_connect.enable()
 
-    print "***** show ip configuration of Device *****"
+    print("***** show ip configuration of Device *****")
     output = net_connect.send_command("show ip int b")
-    print output
+    print(output)
 
     net_connect.disconnect()
 
 except NetMikoTimeoutException:
-    print "================ SOMETHING WRONG HAPPEN WITH {0} ==================".format(device['ip'])
+    print("================ SOMETHING WRONG HAPPEN WITH {0} ==================".format(
+        device['ip']))
 
 except AuthenticationException:
-    print "================ Authentication Failed with {0} ====================".format(device['ip'])
+    print("================ Authentication Failed with {0} ====================".format(
+        device['ip']))
 
 except Exception as unknown_error:
-    print "================ SOMETHING UNKNOWN HAPPEN WITH {0} ================"
+    print(
+        "================ SOMETHING UNKNOWN HAPPEN WITH {0} ================".format(
+            device['ip']))
